0bbebaca979b2cd9225e12ab54a7947f59cc904e
Victor Nicacio <victorcnicacio@hotmail.com>
2024-10-13 18:43:10 -0300

    Edição funcional (CRUD completo)

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -3,8 +3,6 @@ import axios from 'axios'
 
 const Todo = ({ todo, allTodos, setTodos }) => {
 
-    const [style, setStyle] = useState(false)
-
     const locale = 'pt-br'
     const today = new Date().toLocaleDateString(locale)
     const date = new Date(todo.datavenc.split('T')[0]).toLocaleDateString(locale)
@@ -22,9 +20,39 @@ const Todo = ({ todo, allTodos, setTodos }) => {
         const response = await axios.delete(`http://localhost:8080/tarefas/${todo.id}`)
     }
 
+    const [style, setStyle] = useState(true)
+
+    const toggleForm = () =>{
+        setStyle(!style)
+    }
+
+    const [formData, setFormData] = useState({
+        titulo: todo.titulo,
+        descricao: todo.descricao,
+        datavenc: todo.datavenc
+    })
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleEdition = async (e) => {
+        e.preventDefault()
+        const newTodos = [...allTodos]
+        newTodos.map(item => item.id == todo.id ? (
+            item.titulo = formData.titulo,
+            item.descricao = formData.descricao,
+            item.datavenc = formData.datavenc
+        ) : item)
+        toggleForm()
+        setTodos(newTodos)
+        const response = await axios.put(`http://localhost:8080/tarefas/${todo.id}`, formData)
+    }
+
   return (
-    <div className={!todo.jacompleta ? 'todo-item' : 'todo-completed'} id={todo.id}>
-              <div className="content" style={{textDecoration: todo.jacompleta ? 'line-through' : ''}}>
+        <div>
+            <div className={!todo.jacompleta ? 'todo-item' : 'todo-completed'} id={todo.id} style={{display: style ? 'flex' : 'none'}}>
+              <div className="content" style={{textDecoration: todo.jacompleta ? 'line-through' : ''}} onClick={toggleForm} >
                 <p className='todoTitle'>{todo.titulo}</p>
                 <p className='desc'>Descrição: {todo.descricao}</p>
                 <p className='datavenc'>Vencimento: {date >= today ? date : 'Vencido'}</p>
@@ -34,6 +62,20 @@ const Todo = ({ todo, allTodos, setTodos }) => {
                 <button onClick={handleDelete}>Excluir</button>
               </div>
             </div>
+            <form className={!todo.jacompleta ? 'todo-item' : 'todo-completed'} 
+                style={{display: !style ? 'flex' : 'none'}} 
+                onSubmit={handleEdition}>
+                <div className="editForm">
+                    <input type="text" name='titulo' value={formData.titulo} onChange={handleChange}/>
+                    <input type="text" name='descricao' value={formData.descricao} onChange={handleChange}/>
+                    <input type="date" name='datavenc' value={formData.datavenc} onChange={handleChange}/>
+                </div>
+                <div>
+                    <button type='submit' onSubmit={handleEdition}>Confirmar</button>
+                    <button type='button' onClick={toggleForm}>Cancelar</button>
+                </div>
+            </form>
+        </div>
   )
 }
 
