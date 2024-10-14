e8f966a63682eb02fb675fee2e12985c77550721
Victor Nicacio <victorcnicacio@hotmail.com>
2024-10-13 21:14:54 -0300

    Reestruturação do projeto de acordo com as responsabilidades + incluso comentários nos arquivos

--- server/server.js
+++ server/server.js
@@ -1,80 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const client = require('./model/databasepg');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const corsOptions = {
     origin: ["http://localhost:5173"]
-}
+};
 
-app.use(bodyParser.json())
+// Importa o controller
+const router = require('./controller/tarefas.controller');
+
+// Middlewares
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
-client.connect();
+// Rotas
+app.use('/tarefas', router);
 
-app.get('/tarefas/', (req, res) => {
-    client.query('Select * from tarefas', (err, result) => {
-        if(!err){
-            res.json(result.rows);
-        }
-    })
-    client.end
-})
-
-app.post('/tarefas', (req, res) => {
-    const tarefa = req.body;
-    client.query(`INSERT INTO public.tarefas (titulo, descricao, datavenc, iduser, jacompleta) VALUES ('${tarefa.titulo}', '${tarefa.descricao}', '${tarefa.datavenc}', 1, false)`, (err) => {
-        if(!err){
-            console.log("Tarefa registrada com sucesso")
-        } else {
-            console.log(err.message)
-        }
-    })
-    client.end
-})
-
-app.put('/tarefas/:id', (req, res) => {
-    const formInfo = req.body;
-    console.log(formInfo)
-
-    client.query(`update tarefas 
-                set titulo = '${formInfo.titulo}',
-                descricao = '${formInfo.descricao}',
-                datavenc = '${formInfo.datavenc}'
-                where id = ${req.params.id}`, (err) => {
-        if(!err){
-            console.log("Tarefa atualizada com sucesso")
-        } else {
-            console.log(err.message)
-        }
-    })
-    client.end
-})
-
-app.put('/tarefas/:id/:iscompleted', (req, res) => {
-    const userid = req.params.id
-    const isCompleted = req.params.iscompleted
-    client.query(`update tarefas set jacompleta = '${isCompleted=='false' ? true : false}' where id = ${userid}`, (err) => {
-        if(!err){
-            console.log("Tarefa atualizada com sucesso")
-        } else {
-            console.log(err.message)
-        }
-    })
-    client.end
-})
-
-app.delete('/tarefas/:id', (req, res) => {
-    client.query(`delete from tarefas where id=${req.params.id}`, (err) => {
-        if(!err){
-            console.log('Tarefa excluida com sucesso')
-        } else {
-            console.log(err.message)
-        }
-    })
-    client.end
-})
-
-app.listen(8080, ()=> {
-    console.log("Listening on port 8080")
-})
+// Servidor
+app.listen(8080, () => {
+    console.log("Listening on port 8080");
+});
